6ed37be3298175dbfc381d3efb0b4a35d630721d
Bruno Rocha <rochacbruno@gmail.com>
2021-08-12 20:46:42 +0100

    Fix CI 🚢

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="smarter",
-    version="0.1.1",
+    version="0.1.2",
     description="Some objects could be smarter",
     author="Bruno Rocha",
     packages=find_packages(exclude=["tests"]),
