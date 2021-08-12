e2d8475345faab6dcc27a7260c3cfda3e435001c
Bruno Rocha <rochacbruno@gmail.com>
2021-08-12 21:06:59 +0100

    0.1.4 🚀

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="smarter",
-    version="0.1.3",
+    version="0.1.4",
     description="Some objects could be smarter",
     long_description=Path("README.md").read_text(),
     long_description_content_type="text/markdown",
